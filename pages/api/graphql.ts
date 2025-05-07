46a92643d716a7b77788cb7cc1ca3f4aeb65e828
Cullen McLaren <cullenmclaren@gmail.com>
2025-05-07 11:30:37 -0400

    add axios, grab spacex launch data

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -4,6 +4,7 @@ import { NextRequest } from "next/server";
 import path from "path";
 import fs from "fs";
 import gql from "graphql-tag";
+import axios from "axios";
 
 const schemaPath = path.join(process.cwd(), "graphql", "schema.gql");
 
@@ -14,6 +15,33 @@ console.log("typeDefs", typeDefs);
 const resolvers = {
   Query: {
     hello: () => "yep that works",
+    // Maybe switch to grabbing the json from their official REST endpoint later https://github.com/r-spacex/SpaceX-API
+    // TODO: Store locally and update occasionally
+    launches: async () => {
+      try {
+        const response = await axios.post(
+          "https://spacex-production.up.railway.app/",
+          {
+            query: `
+            {
+                launches {
+                mission_name
+                launch_date_utc
+                rocket {
+                    rocket_name
+                    rocket_type
+                    }
+                }
+            }`,
+          }
+        );
+
+        return response.data.data.launches;
+      } catch (error) {
+        console.error("Error fetching launches:", error);
+        throw new Error("Failed to fetch launches");
+      }
+    },
   },
 };
 
