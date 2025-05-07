48767e5f91d4dce5cc18c803e16cb009845e33fb
Cullen McLaren <cullenmclaren@gmail.com>
2025-05-07 11:06:39 -0400

    add graphql and apollo server

---
+++ pages/api/graphql.ts
@@ -0,0 +1,22 @@
+import { ApolloServer } from "@apollo/server";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { NextRequest } from "next/server";
+import path from "path";
+import fs from "fs";
+import gql from "graphql-tag";
+
+const schemaPath = path.join(process.cwd(), "graphql", "schema.gql");
+
+const typeDefs = gql(fs.readFileSync(schemaPath, "utf-8"));
+
+console.log("typeDefs", typeDefs);
+
+const resolvers = {
+  Query: {
+    hello: () => "yep that works",
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+export default startServerAndCreateNextHandler<NextRequest>(server);
